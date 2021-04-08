ed543240e17f28bdaff2af09119a503e933ef11a
Charles Tuggle <tuggle.charles@gmail.com>
2021-04-08 12:47:50 -0400

    created new js file to include fetch for the europeana collection.

---
+++ js/europeana_fetch.js
@@ -0,0 +1,27 @@
+// JS logic for searching Europeana
+
+// returns matching art objects from keyword search
+function searchEuropeana() {
+  const query = $(".search-input-europeana").val().trim();
+
+  fetch("https://api.europeana.eu/record/v2/search.json?wskey=anduumbit&MEDIA=true&query=" + query)
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      console.log(data);
+      // displayArtEuropeana(data);
+    });
+}
+
+// formats searchArt() results via DOM manipulation
+function displayEuropeana(data) {
+  // for (let i = 0; i < data.artObjects.length; i++) {
+  //   const art = document.querySelector(".art-div");
+  //   const artImage = document.createElement("img");
+  //   artImage.classList.add("flex-item");
+  //   artImage.src = data.artObjects[i].webImage.url;
+  //   artImage.alt = data.artObjects[i].longTitle;
+  //   art.appendChild(artImage);
+  // }
+}
